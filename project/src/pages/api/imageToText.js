360935ecf7a5d42d2cee34d0bf271a7b38f5da66
Jesse Xia <68565085+x-jesse@users.noreply.github.com>
2024-11-10 02:19:23 -0500

    Refactor api

---
+++ project/src/pages/api/imageToText.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+import FormData from 'form-data';
+
+export default async function simpletexHandler(fileStream, filename) {
+  const SIMPLETEX_API_TOKEN = process.env.SIMPLETEX_API_TOKEN;
+  const apiUrl = "https://server.simpletex.cn/api/simpletex_ocr";
+
+  const formData = new FormData();
+  formData.append("file", fileStream, filename);
+  const response = await axios.post(apiUrl, formData, {
+    headers: {
+      ...formData.getHeaders(),
+      token: SIMPLETEX_API_TOKEN,
+    },
+  });
+
+  return response.data;
+}
