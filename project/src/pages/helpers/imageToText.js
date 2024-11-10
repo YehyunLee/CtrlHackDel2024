465ef76bec87ae69652efb4b92282d935b454bda
Jesse Xia <68565085+x-jesse@users.noreply.github.com>
2024-11-10 02:06:24 -0500

    Update conflict

--- project/src/pages/helpers/imageToText.js
+++ project/src/pages/helpers/imageToText.js
@@ -1,19 +1,12 @@
 import axios from 'axios';
-import fs from 'fs';
 import FormData from 'form-data';
 
-export async function simpletexHandler(files) {
+export async function simpletexHandler(fileStream, filename) {
   const SIMPLETEX_API_TOKEN = process.env.SIMPLETEX_API_TOKEN;
   const apiUrl = "https://server.simpletex.cn/api/simpletex_ocr";
 
-  const file = Array.isArray(files.file) ? files.file[0] : files.file;
-  if (!file || !file.filepath) {
-    return res.status(400).json({ message: 'No file uploaded or file path is missing' });
-  }
-  const fileStream = fs.createReadStream(file.filepath);
-
   const formData = new FormData();
-  formData.append("file", fileStream, file.originalFilename);
+  formData.append("file", fileStream, filename);
   const response = await axios.post(apiUrl, formData, {
     headers: {
       ...formData.getHeaders(),
