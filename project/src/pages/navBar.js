3d4e33297fcbdb3603ee87abe595e95f0876a70f
Yehyun <glad7cu@gmail.com>
2024-11-10 12:40:41 -0500

    New Logo

--- project/src/pages/navBar.js
+++ project/src/pages/navBar.js
@@ -7,8 +7,8 @@ export default function Navbar() {
     <nav className="bg-gray-900 border-b border-gray-800">
       <div className="max-w-7xl mx-auto px-4">
         <div className="h-16 flex items-center">
-          <Image src="/Logo.png" alt="Logo" width={32} height={32} />
-          <span className="text-white font-semibold text-xl">MagicQuill</span>
+          <Image src="/Logo.png" alt="Logo" width={128} height={64*2} />
+          {/* <span className="text-white font-semibold text-xl">MagicQuill</span> */}
         </div>
       </div>
     </nav>
